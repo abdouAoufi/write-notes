ab2b95a13f51457deecbee0ceb51045a44a349a7
Abderahmane Aoufi <abdouou7@gmail.com>
2021-07-27 18:05:55 +0100

    working on add note

---
+++ test/array.js
@@ -0,0 +1,20 @@
+// [1, 2, 3].forEach((elemen, index) => {
+//   console.log(elemen, index);
+// });
+
+
+
+
+
+
+((diamiter , PI) => PI * diamiter )(2 , 3.1434355) // output 6.286871
+
+((diamiter) => 3.1434355 * diamiter)(2); // output 6.286871
+
+((PI) => (diamiter) => PI * diamiter)(3.1434355)(2);  // output 6.286871
+
+((diamiter) => (PI) => PI * diamiter )(3.1434355)(2);  // output 6.286871
+
+((diamiter) => { const PI = 3.1434355 ; return PI * diamiter; })(2);  // output 6.286871 ( side effect :(  ) 
+
+
