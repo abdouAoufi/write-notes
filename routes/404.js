288a5ac764697a18e93f29838c6989081672381f
AbdouAoufi <Abdouou7@gmail.com>
2021-08-25 10:36:50 +0100

    fix grid view

--- routes/404.js
+++ routes/404.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use((req , res) => {
-   res.status(404).render("404.ejs");
+   res.redirect("/home")
 })
 
 module.exports = router;
